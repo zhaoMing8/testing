26434f6cd9fc3a7e1c6d3d3203b740f16602edab
zhaoMing8 <ip188520@163.com>
2020-03-14 21:17:08 +0800

    创建testing项目

---
+++ products/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    quantity = models.IntegerField()
+    published_on = models.DateField()
+
+    @property
+    def is_in_stock(self):
+        return self.quantity > 0
---
+++ products/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Product
+
+
+# Create your views here.
+@login_required
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'product_detail.html', {'product': product})
