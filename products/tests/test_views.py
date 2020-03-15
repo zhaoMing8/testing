d0a277a69058620168f107593fc9c233d4f72a43
zhaoMing8 <ip188520@163.com>
2020-03-15 13:20:32 +0800

    使用fixture初始化数据

--- products/tests/test_views.py
+++ products/tests/test_views.py
@@ -3,26 +3,35 @@ from django.urls import reverse
 from django.contrib.auth.models import User, AnonymousUser
 from products.views import product_detail
 from mixer.backend.django import mixer
+from django.test import TestCase
 import pytest
 
 
-@pytest.mark.django_db
-class TestView:
+@pytest.fixture
+def factory():
+    return RequestFactory()
 
-    def test_product_detail_authenticated(self):
-        mixer.blend('products.Product')
-        path = reverse('detail', kwargs={'pk': 1})
-        request = RequestFactory().get(path)
-        request.user = mixer.blend(User)
 
-        response = product_detail(request, pk=1)
-        assert response.status_code == 200
-    
-    def test_product_detail_unauthenticated(self):
-        mixer.blend('products.Product')
-        path = reverse('detail', kwargs={'pk': 1})
-        request = RequestFactory().get(path)
-        request.user = AnonymousUser()
+@pytest.fixture
+def product(db):
+    return mixer.blend('products.Product')
 
-        response = product_detail(request, pk=1)
-        assert 'accounts/login' in response.url
+
+def test_product_detail_authenticated(factory, product):
+    # mixer.blend('products.Product')
+    path = reverse('detail', kwargs={'pk': 1})
+    request = factory.get(path)
+    request.user = mixer.blend(User)
+
+    response = product_detail(request, pk=1)
+    assert response.status_code == 200
+
+
+def test_product_detail_unauthenticated(factory, product):
+    # mixer.blend('products.Product')
+    path = reverse('detail', kwargs={'pk': 1})
+    request = factory.get(path)
+    request.user = AnonymousUser()
+
+    response = product_detail(request, pk=1)
+    assert 'accounts/login' in response.url
